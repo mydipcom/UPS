3ab17406cf3e9067c9dc224c4012f03087c7b07e
phillsli-campray <phills.li@campray.com>
2014-11-29 11:36:38 +0800

    Modified the system config

--- BPS/src/main/java/com/bps/commons/SystemConfig.java
+++ BPS/src/main/java/com/bps/commons/SystemConfig.java
@@ -1,6 +1,7 @@
 package com.bps.commons;
 
 import java.util.Hashtable;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -11,11 +12,11 @@ public class SystemConfig {
 		
 	public static String Api_Access_Key;
 	
-	public static Map<String,Long> Admin_Nodes_Url_Map=new Hashtable<String,Long>();
+	public static Map<String,Long> Admin_Nodes_Url_Map=new LinkedHashMap<String,Long>();
 					
-	public static Map<TadminNodes,List<TadminNodes>> Admin_Nodes_Menu_Map=new Hashtable<TadminNodes,List<TadminNodes>>();
+	public static Map<TadminNodes,List<TadminNodes>> Admin_Nodes_Menu_Map=new LinkedHashMap<TadminNodes,List<TadminNodes>>();
 	
-	public static Map<String,List<TadminNodes>> Admin_Nodes_Group_Map=new Hashtable<String,List<TadminNodes>>();
+	public static Map<String,List<TadminNodes>> Admin_Nodes_Group_Map=new LinkedHashMap<String,List<TadminNodes>>();
 	
 	public static Map<String,String> Admin_Setting_Map=new Hashtable<String,String>();
 		
