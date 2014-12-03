1dc51efec17f0c059e5ac9e87831f0049cfbd0cd
Silva <silva.song@campray.com>
2014-12-03 11:57:54 +0800

    modify the codes

---
+++ BPS/src/main/java/com/bps/model/InterfaceInfoAnnotation.java
@@ -0,0 +1,15 @@
+package com.bps.model;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface InterfaceInfoAnnotation {
+	String name();
+
+	String description();
+
+}
